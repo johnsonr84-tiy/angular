5ab96009c54e0728fc56f60584fc3e93ca45e6a9
codedork1 <robertcjohnson1984@gmail.comgit config --global user.emailgit config --global user.email robertcjohnson1984@gmail.com>
2017-04-25 10:53:24 -0600

    chore (main) first commit

---
+++ 01-basic/scripts/app.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const app = angular.module("ourApp", [])
+
+app.controller("FirstCtrl", First)
+
+function First ($scope) {
+  console.log("main controller to groundcontol");
+
+  let now = new Date()
+  $scope.now = now.toTimeString();
+  console.log('$scope.now is:', $scope.now);
+
+  $scope.updateTime = function() {
+    let now = new Date()
+    $scope.now = now.toTimeString()
+  }
+}
+
+app.controller("BindingController", ['$scope', function($scope) {
+
+}])
---
+++ 02-model-review/scripts/app.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const app = angular.module("ngApp",[])
+
+app.controller("ExampleCtrl", ExCtrl)
+
+function ExCtrl ($scope) {
+  $scope.txtChange = function() {
+    console.log('ng-change called')
+  }
+}
+
+app.controller("Ctrl1", CtrlOne);
+app.controller("Ctrl2", CtrlTwo);
+
+function CtrlOne ($scope) {
+  $scope.testVar = "testVar inside CtrlOne"
+}
+
+function CtrlTwo ($scope) {
+  $scope.testVar = "testVar inside CtrlTwo"
+}
